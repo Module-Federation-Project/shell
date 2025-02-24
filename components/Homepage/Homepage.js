1a93c0767a5593a6a223f8794516fe86887508f0
Helin Okcu <helinnokcu@gmail.com>
2025-02-24 14:16:15 +0300

    header changed

--- components/Homepage/Homepage.js
+++ components/Homepage/Homepage.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Homepage = () => {
   return (
     <div className="container">
-      <h1>Welcome to homepage</h1>
+      <h1>Welcome!</h1>
+      <h2>This is your shell app.</h2>
     </div>
   );
 };
